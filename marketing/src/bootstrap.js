7e090b3f05ca74dc56865df96c70ea71cace5b1e
rjanapal <rakesh.kumar.janapalli@yara.com>
2022-08-05 16:09:36 +0530

    Demo project for micro front end using react

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+
+const mount=(el)=>{
+    ReactDom.render(
+        <App/>,el);
+}
+
+if(process.env.NODE_ENV=='development'){
+   const el = document.querySelector('#marketing-app');
+   if(el){
+       mount(el);
+   }
+}
+
+export {mount};
