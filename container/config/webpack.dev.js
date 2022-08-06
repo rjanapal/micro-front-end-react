3adb2e6728f216da6ec97724ed38887cc8828872
rjanapal <rakesh.kumar.janapalli@yara.com>
2022-08-06 15:36:22 +0530

    Add webpack prod config changes

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
@@ -18,10 +18,7 @@ const devConfig = {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
       },
       shared:['react','react-dom']
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
+    })
   ],
 };
 
